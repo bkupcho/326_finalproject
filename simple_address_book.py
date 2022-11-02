6b37c19e1e8f2da667679cc39666a6afe5e9aa4a
Dek700 <112531730+Dek700@users.noreply.github.com>
2022-11-02 08:28:42 -0400

    Update simple_address_book.py

--- simple_address_book.py
+++ simple_address_book.py
@@ -4,3 +4,19 @@ class Filters:
         self.data = data
         self.name = name
      
+class School:
+    """ This is a class representing a school in our database """
+    
+    def __init__(self, name, address, total_enrollment, rating):
+        self.name = name
+        self.address = address
+        self.enrollment = total_enrollment
+        self.rating = rating:
+
+class ElementarySchool:
+    """ This is a class representing an elementary school in our database"""
+    
+    def __init__(self, name, address, total_enrollment, rating, grades):
+        super().__init__(name, address, total_enrollment, rating)
+        self.grades = grades
+        
