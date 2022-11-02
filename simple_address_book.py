ec427a15732f11b09c2d5681ceccc730b97adc59
bkupcho <112403258+bkupcho@users.noreply.github.com>
2022-11-02 00:08:25 -0400

    Update simple_address_book.py
    added name attribute

--- simple_address_book.py
+++ simple_address_book.py
@@ -1,5 +1,6 @@
 class Filters:
     """ A filtering system for an collection of people and their information """
-    def __init__(self, data):
+    def __init__(self, name, data):
         self.data = data
+        self.name = name
      
