8ea137bc6e8881bcaae4970a1cbf7341b7bcfde9
bkupcho <112403258+bkupcho@users.noreply.github.com>
2022-11-02 00:13:28 -0400

    Update simple_address_book.py

--- simple_address_book.py
+++ simple_address_book.py
@@ -1,6 +1,7 @@
 class Filters:
     """ A filtering system for an collection of people and their information """
     def __init__(self, name, data):
-        self.data = data
         self.name = name
+        self.data = data
+      
      
