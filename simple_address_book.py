3707a93f98bc8d226bef79319eb4c5f1a5e2f5c7
shreeyapin <112404434+shreeyapin@users.noreply.github.com>
2022-11-18 19:46:17 -0500

    Update simple_address_book.py

--- simple_address_book.py
+++ simple_address_book.py
@@ -1,33 +1,3 @@
-class Filters:
-    """ A filtering system for an collection of people and their information """
-    def __init__(self, name, address, phone_num):
-        self.name = name
-        self.address = address
-        self.phone_num = phone_num
-     
-class School:
-    """ This is a class representing a school in our database """
-    
-    def __init__(self, name, address, total_enrollment, rating):
-        self.name = name
-        self.address = address
-        self.enrollment = total_enrollment
-        self.rating = rating:
-
-class ElementarySchool(School):
-    """ This is a class representing an elementary school in our database"""
-    
-    def __init__(self, name, address, total_enrollment, rating, grades):
-        super().__init__(name, address, total_enrollment, rating)
-        self.grades = grades
-        
-class Contact:
-    """A class displaying a users' contact information."""
-    def __init__(self, name, phone_num, birthday):
-        self.name = name
-        self.phone_num = phone_num
-        self.birthday = birthday
-        
 class Person:
     """Class representing a person and their information.
 
@@ -73,5 +43,24 @@ class Person:
 	A dataframe consisting of the people in a given county
     """
 	# creates a sorted dataframe that only shows people who live in a given county
+	
+class ResourceDatabase:
+''' Class representing the resources within a county
+Attributes: 
+	database (Set) '''
 
-     
+	def __init__(self, filepath):
+	''' For each 'line’ in file being read in
+	expression = r “ … “
+			New_resource = Resource
+			Here, I’m going to create a new ‘Resource’ object for each line, then 
+	Read in the file with data about local nearby resources, these include:
+	Schools
+	Health Centers / Hospitals ''' 
+	
+	with open ( … ) as f:
+
+	def __str__(self):
+
+
+	resources_by_town(String: town_name):
